ce7ada82bd748c151f4516c809b6fefed444fdcf
Caffin <caffinmanon@gmail.com>
2024-01-04 15:24:17 +0100

    page index front ok

---
+++ pages/visit_two.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="../styles.css" rel="stylesheet">
+    <title>Instagram</title>
+</head>
+
+<body>
+
+    <main>
+
+        <div class="d-flex vh-100">
+
+            <div class="blockInsta col-2 p-5">
+                <div class="d-flex justify-content-start">
+                    <h1 class="text center">Instagram</h1>
+                </div>
+
+                <div class="pt-5">
+                    <p>Accueil</p>
+                    <br>
+                    <p>Découvrir</p>
+                    <br>
+                    <p>Messages</p>
+                    <br>
+                    <p>Créer</p>
+                    <br>
+                    <p>Profil</p>
+                    <br>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Rechercher" aria-label="Search">
+                        <button class="btn btn-outline-danger" type="submit">Rechercher</button>
+                </div>
+            </div>
+
+            <div class="container-md col-7 p-5">
+
+                <div class="d-flex justify-content-around">
+                    <img src="../img/insta4.jpg" class="rounded-circle" alt="Logo HTML w3" style="width:auto; height:70%">
+
+                    <div class="flex-colum">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <p class="pe-3">Arnaud Malras</p>
+
+                            <div class="justify-content-between pb-3">
+                                <button type="button" class="btn btn-secondary btn-sm">Suivre</button>
+                                <button type="button" class="btn btn-secondary btn-sm">contacter</button>
+                            </div>
+
+                            <!-- <div class="justify-content-start">
+                                <div>
+                                    <p class="text-center pt-2">44</p>
+                                    <p class="text-center">publications</p>
+                                </div>
+                                <div>
+                                    <p class="text-center pt-2">736</p>
+                                    <p class="text-center">followers</p>
+                                </div>
+                                <div>
+                                    <p class="text-center pt-2">290</p>
+                                    <p class="text-center">Suivi(e)s</p>
+                                </div>
+                            </div> -->
+                        </div>
+
+                        <div>
+                            <p>Ébéniste à Lille</p>
+                            <p>Conception et fabrication sur mesure</p>
+                            <p>Mobilier agencement d’espaces</p>
+                        </div>
+
+                        <div class="col-12 d-flex justify-content-between align-items-center text-center pt-3">
+                            <div>
+                                <img src="../img/insta4.jpg" class="rounded-circle" alt="Logo HTML w3" style="width:auto; height:70px">
+                                <p class="">Fabrication</p>
+                            </div>
+                            <div>
+                                <img src="../img/insta4.jpg" class="rounded-circle" alt="Logo HTML w3" style="width:auto; height:70px">
+                                <p>Bois</p>
+                            </div>
+                            <div>
+                                <img src="../img/insta4.jpg" class="rounded-circle" alt="Logo HTML w3" style="width:auto; height:70px">
+                                <p>Finition</p>
+                            </div>
+                            <div>
+                                <img src="../img/insta4.jpg" class="rounded-circle" alt="Logo HTML w3" style="width:auto; height:70px">
+                                <p>Enseignement</p>
+                            </div>
+                            <div>
+                                <img src="../img/insta4.jpg" class="rounded-circle" alt="Logo HTML w3" style="width:auto; height:70px">
+                                <p>Stage</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container-fluid d-flex w-100 pt-5">
+                    <div class="row">
+                        <div class="col-7 pb-3">
+                            <img src="../img/insta2.jpg" style="width:15vw; height:25vh">
+                        </div>
+
+                        <div class="col-7 pb-3">
+                            <img src="../img/pleins-logo-insta.jpeg" style="width:15vw; height:25vh">
+                        </div>
+
+                        <div class="col-7 pb-3">
+                            <img src="../img/insta3.jpg" style="width:15vw; height:25vh">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-7 pb-3">
+                            <img src="../img/insta1.jpg" style="width:15vw; height:25vh">
+                        </div>
+
+                        <div class="col-7 pb-3">
+                            <img src="../img/insta2.jpg" style="width:15vw; height:25vh">
+                        </div>
+
+                        <div class="col-7 pb-3">
+                            <img src="../img/pleins-logo-insta.jpeg" style="width:15vw; height:25vh">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-7 pb-3">
+                            <img src="../img/insta2.jpg" style="width:15vw; height:25vh">
+                        </div>
+
+                        <div class="col-7 pb-3">
+                            <img src="../img/insta1.jpg" style="width:15vw; height:25vh">
+                        </div>
+
+                        <div class="col-7 pb-3">
+                            <img src="../img/pleins-logo-insta.jpeg" style="width:15vw; height:25vh">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+    </main>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
