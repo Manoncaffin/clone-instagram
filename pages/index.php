45186b7621999adbb872cebbd3504365c6ab7e5d
Caffin <caffinmanon@gmail.com>
2024-01-04 12:20:59 +0100

    page index en cours

--- pages/index.php
+++ pages/index.php
@@ -1,16 +1,141 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="./styles.css" rel="stylesheet">
+    <link href="../styles.css" rel="stylesheet">
     <title>Instagram</title>
 </head>
+
 <body>
-    
+
+    <main>
+
+        <div class="d-flex vh-100">
+
+            <div class="blockInsta col-2 p-5">
+                <div class="d-flex justify-content-start">
+                    <h1 class="text center">Instagram</h1>
+                </div>
+
+                <div class="pt-5">
+                    <p>Accueil</p>
+                    <br>
+                    <p>Recherche</p>
+                    <br>
+                    <p>Découvrir</p>
+                    <br>
+                    <p>Messages</p>
+                    <br>
+                    <p>Créer</p>
+                    <br>
+                    <p>Profil</p>
+                </div>
+            </div>
+
+            <div class="container-md col-7 p-5">
+
+                <div class="d-flex justify-content-between">
+                    <div class="justify-content-start">
+                        <img src="../img/insta4.jpg" class="rounded-circle" alt="Logo HTML w3" style="width:auto; height:70%">
+                    </div>
+
+                    <div class="d-flex flex-column align-items-start">
+                        <p class="">Clément Gaillard designer</p>
+
+                        <div class="align-items-center">
+                            <button type="button" class="btn btn-secondary">Modifier le profil</button>
+                            <button type="button" class="btn btn-secondary">Voir l'archive</button>
+                        </div>
+
+                        <div class="d-flex">
+                            <p class="text-center p-3">44</p>
+                            <p class="text-center p-3">publications</p>
+
+                            <p class="text-center p-3">736</p>
+                            <p class="text-center p-3">followers</p>
+
+                            <p class="text-center p-3">290</p>
+                            <p class="text-center p-3">Suivi(e)s</p>
+                        </div>
+
+                        <div>
+                        <p>Designer, Urbanist & Consultant in Bioclimatic design</p>
+                        <p>Fondateur : @freio_designclimatique</p>
+                        <p>Voir la traduction</p>
+                        <a href="">clementgaillard.com</a>
+                    </div>
+
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-around align-items-center w-100 pt-5">
+
+                </div>
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
+
+                
+                    <div class="container-fluid d-flex">
+                        <div class="row">
+                            <div class="col-7">
+                                <img src="../img/insta2.jpg">
+                            </div>
+
+                            <div class="col-7">
+                                <img src="../img/insta2.jpg">
+                            </div>
+
+                            <div class="col-7">
+                                <img src="../img/insta2.jpg">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-7">
+                                <img src="../img/insta2.jpg">
+                            </div>
+
+                            <div class="col-7">
+                                <img src="../img/insta2.jpg">
+                            </div>
+
+                            <div class="col-7">
+                                <img src="../img/insta2.jpg">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-7">
+                                <img src="../img/insta2.jpg">
+                            </div>
+
+                            <div class="col-7">
+                                <img src="../img/insta2.jpg">
+                            </div>
+
+                            <div class="col-7">
+                                <img src="../img/insta2.jpg">
+                            </div>
+                        </div>
+                    </div>
+
+                
+            </div>
+
+
+
+
+
+    </main>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>
